fc962029102231411490aba42000f22af117943d
suryaansh132004 <suryaanshguleria@gmail.com>
2026-07-06 12:54:27 +0530

    remove unsafe-eval from CSP

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,7 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+      "script-src 'self' 'unsafe-inline'",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: blob: https:",
